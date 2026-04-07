525119fc78a8e28f79f9e87ddf4874b96dbdd26e
C-arl-os <3636514110101@ingenieria.usac.edu.gt>
2026-04-06 22:33:07 -0600

    feat: configurar servidor y conexion a base de datos

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const pool = require('./src/config/database');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+pool.connect()
+    .then(() => {
+        console.log('Connected to the database');   
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database', err);
+    });
+
